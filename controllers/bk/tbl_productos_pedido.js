2b7fd51560cbcab44fbec9d3b2e6def4de8f5bb9
darwinluque <darwin.luque@gmail.com>
2018-08-04 13:54:06 -0500

    Add files via upload

---
+++ controllers/bk/tbl_productos_pedido.js
@@ -0,0 +1,143 @@
+const tbl_parametros = require('../models').tbl_parametros;
+const tbl_artista = require('../models').tbl_artista;
+const tbl_canciones = require('../models').tbl_canciones;
+const tbl_cartas = require('../models').tbl_cartas;
+const tbl_discotecas = require('../models').tbl_discotecas;
+const tbl_encuesta = require('../models').tbl_encuesta;
+const tbl_eventos = require('../models').tbl_eventos;
+const tbl_funcionarios = require('../models').tbl_funcionarios;
+const tbl_generos_fav = require('../models').tbl_generos_fav;
+const tbl_listas = require('../models').tbl_listas;
+const tbl_mesas = require('../models').tbl_mesas;
+const tbl_pedidos = require('../models').tbl_pedidos;
+const tbl_personas = require('../models').tbl_personas;
+const tbl_personas_discoteca = require('../models').tbl_personas_discoteca;
+const tbl_pqrs = require('../models').tbl_pqrs;
+const tbl_precios_boletas = require('../models').tbl_precios_boletas;
+const tbl_productos_pedido = require('../models').tbl_productos_pedido;
+const tbl_productos = require('../models').tbl_productos;
+const tbl_productos_carta = require('../models').tbl_productos_carta;
+const tbl_promociones = require('../models').tbl_promociones;
+const tbl_reservas = require('../models').tbl_reservas;
+const tbl_servicios_discoteca = require('../models').tbl_servicios_discoteca;
+const tbl_suscriptores = require('../models').tbl_suscriptores;
+const tbl_votos_canciones = require('../models').tbl_votos_canciones;
+
+
+module.exports = {
+  list(req, res) {
+    return tbl_productos_pedido
+      .findAll({
+        include: [{
+            model: tbl_pedidos,
+            as: 'pedidos'
+        },{
+            model: tbl_productos,
+            as: 'productos'
+        },{
+            model: tbl_discotecas,
+            as: 'discotecas'
+        }],
+        order: [
+          ['createdAt', 'DESC'],
+          //[{ model: tbl_discotecas, as: 'vestuarios' }, 'createdAt', 'DESC'],
+        ],
+      })
+      .then((tbl_productos_pedido) => res.status(200).send(tbl_productos_pedido))
+      .catch((error) => { res.status(400).send(error); });
+  },
+
+  getById(req, res) {
+    return tbl_productos_pedido
+      .findById(req.params.id, {
+        include: [{
+            model: tbl_pedidos,
+            as: 'pedidos'
+        },{
+            model: tbl_productos,
+            as: 'productos'
+        },{
+            model: tbl_discotecas,
+            as: 'discotecas'
+        }],
+      })
+      .then((tbl_productos_pedido) => {
+        if (!tbl_productos_pedido) {
+          return res.status(404).send({
+            message: 'tbl_productos_pedido Not Found',
+          });
+        }
+        return res.status(200).send(tbl_productos_pedido);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  add(req, res) {
+    return tbl_productos_pedido
+      .create({
+        id_pedido: req.body.id_pedido,
+        id_discoteca: req.body.id_discoteca,
+        id_producto: req.body.id_producto,
+        num_cantidad: req.body.num_cantidad,
+        vlr_costo: req.body.vlr_costo,
+      })
+      .then((tbl_productos_pedido) => res.status(201).send(tbl_productos_pedido))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return tbl_productos_pedido
+      .findById(req.params.id, {
+        include: [{
+            model: tbl_pedidos,
+            as: 'pedidos'
+        },{
+            model: tbl_productos,
+            as: 'productos'
+        },{
+            model: tbl_discotecas,
+            as: 'discotecas'
+        }],
+      })
+      .then(tbl_productos_pedido => {
+        if (!tbl_productos_pedido) {
+          return res.status(404).send({
+            message: 'tbl_productos_pedido Not Found',
+          });
+        }
+        return tbl_productos_pedido
+          .update({
+            id_pedido: req.body.id_pedido,
+            id_discoteca: req.body.id_discoteca,
+            id_producto: req.body.id_producto,
+            num_cantidad: req.body.num_cantidad,
+            vlr_costo: req.body.vlr_costo,
+          })
+          .then(() => res.status(200).send(tbl_productos_pedido))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  delete(req, res) {
+    return tbl_productos_pedido
+      .findById(req.params.id)
+      .then(tbl_productos_pedido => {
+        if (!tbl_productos_pedido) {
+          return res.status(400).send({
+            message: 'tbl_productos_pedido Not Found',
+          });
+        }
+        return tbl_productos_pedido
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+};
+
+
+
+
+
---
+++ controllers/tbl_pqrs.js
@@ -0,0 +1,143 @@
+const tbl_parametros = require('../models').tbl_parametros;
+const tbl_artista = require('../models').tbl_artista;
+const tbl_canciones = require('../models').tbl_canciones;
+const tbl_cartas = require('../models').tbl_cartas;
+const tbl_discotecas = require('../models').tbl_discotecas;
+const tbl_encuesta = require('../models').tbl_encuesta;
+const tbl_eventos = require('../models').tbl_eventos;
+const tbl_funcionarios = require('../models').tbl_funcionarios;
+const tbl_generos_fav = require('../models').tbl_generos_fav;
+const tbl_listas = require('../models').tbl_listas;
+const tbl_mesas = require('../models').tbl_mesas;
+const tbl_pedidos = require('../models').tbl_pedidos;
+const tbl_personas = require('../models').tbl_personas;
+const tbl_personas_discoteca = require('../models').tbl_personas_discoteca;
+const tbl_pqrs = require('../models').tbl_pqrs;
+const tbl_precios_boletas = require('../models').tbl_precios_boletas;
+const tbl_productos_pedido = require('../models').tbl_productos_pedido;
+const tbl_productos = require('../models').tbl_productos;
+const tbl_productos_carta = require('../models').tbl_productos_carta;
+const tbl_promociones = require('../models').tbl_promociones;
+const tbl_reservas = require('../models').tbl_reservas;
+const tbl_servicios_discoteca = require('../models').tbl_servicios_discoteca;
+const tbl_suscriptores = require('../models').tbl_suscriptores;
+const tbl_votos_canciones = require('../models').tbl_votos_canciones;
+
+
+module.exports = {
+  list(req, res) {
+    return tbl_pqrs
+      .findAll({
+        /*include: [{
+            model: tbl_discotecas,
+            as: 'discotecas'
+        },{
+            model: tbl_personas,
+            as: 'personas'
+        },{
+            model: tbl_parametros,
+            as: 'tipos_pqr'
+        }],*/
+        order: [
+          ['createdAt', 'DESC'],
+          //[{ model: tbl_discotecas, as: 'vestuarios' }, 'createdAt', 'DESC'],
+        ],
+      })
+      .then((tbl_pqrs) => res.status(200).send(tbl_pqrs))
+      .catch((error) => { res.status(400).send(error); });
+  },
+
+  getById(req, res) {
+    return tbl_pqrs
+      .findById(req.params.id, {
+        /*include: [{
+            model: tbl_discotecas,
+            as: 'discotecas'
+        },{
+            model: tbl_personas,
+            as: 'personas'
+        },{
+            model: tbl_parametros,
+            as: 'tipos_pqr'
+        }],*/
+      })
+      .then((tbl_pqrs) => {
+        if (!tbl_pqrs) {
+          return res.status(404).send({
+            message: 'tbl_pqrs Not Found',
+          });
+        }
+        return res.status(200).send(tbl_pqrs);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  add(req, res) {
+    return tbl_pqrs
+      .create({
+        id_discoteca: req.body.id_discoteca,
+        id_persona: req.body.id_persona,
+        id_tipo_pqr: req.body.id_tipo_pqr,
+        str_descripcion: req.body.str_descripcion,
+        dtm_fecha_pqr: req.body.dtm_fecha_pqr,
+      })
+      .then((tbl_pqrs) => res.status(201).send(tbl_pqrs))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return tbl_pqrs
+      .findById(req.params.id, {
+        /*include: [{
+            model: tbl_discotecas,
+            as: 'discotecas'
+        },{
+            model: tbl_personas,
+            as: 'personas'
+        },{
+            model: tbl_parametros,
+            as: 'tipos_pqr'
+        }],*/
+      })
+      .then(tbl_pqrs => {
+        if (!tbl_pqrs) {
+          return res.status(404).send({
+            message: 'tbl_pqrs Not Found',
+          });
+        }
+        return tbl_pqrs
+          .update({
+            id_discoteca: req.body.id_discoteca,
+            id_persona: req.body.id_persona,
+            id_tipo_pqr: req.body.id_tipo_pqr,
+            str_descripcion: req.body.str_descripcion,
+            dtm_fecha_pqr: req.body.dtm_fecha_pqr,
+          })
+          .then(() => res.status(200).send(tbl_pqrs))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  delete(req, res) {
+    return tbl_pqrs
+      .findById(req.params.id)
+      .then(tbl_pqrs => {
+        if (!tbl_pqrs) {
+          return res.status(400).send({
+            message: 'tbl_pqrs Not Found',
+          });
+        }
+        return tbl_pqrs
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+};
+
+
+
+
+
