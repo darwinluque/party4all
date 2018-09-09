e2ba4e88957110383463145c513b210a1f90e217
darwinluque <darwin.luque@gmail.com>
2018-09-08 20:49:24 -0500

    head
    setHeader

--- routes/index.js
+++ routes/index.js
@@ -33,10 +33,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers','Content-Type');
-  res.header('Access-Control-Allow-Credentials','true');
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token');
+  res.setHeader('Access-Control-Allow-Credentials','true');
+  res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+  res.setHeader('Access-Control-Max-Age', '1000');
   next();
 });
 
