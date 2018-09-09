4749c3aeb57f5fb544c997d9fbc355d6a52e50d9
darwinluque <darwin.luque@gmail.com>
2018-09-08 21:04:40 -0500

    res
    res

--- routes/index.js
+++ routes/index.js
@@ -36,9 +36,9 @@ router.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Methods','*');
   res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token');
-  res.header('Access-Control-Allow-Credentials','true');
-  res.header('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-  res.header('Access-Control-Max-Age', '1000');
+  //res.header('Access-Control-Allow-Credentials','true');
+  //res.header('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+  //res.header('Access-Control-Max-Age', '1000');
   next();
 });
 
