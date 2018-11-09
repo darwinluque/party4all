786c7c4be58543c1336e694abc6a1f590b15ae1e
xdiegob <doego4214@gmail.com>
2018-11-08 19:27:03 -0500

    id lista
    as

--- controllers/tbl_mensajes.js
+++ controllers/tbl_mensajes.js
@@ -142,6 +142,8 @@ module.exports = {
     for(var i=0; i<campos.length; i++){
         var datos = campos[i].split(':');
         whereClause[datos[0]] = datos[1];
+        if(datos[0]=='id_lista')
+          lista = datos[1];
     }
 
     return tbl_mensajes
